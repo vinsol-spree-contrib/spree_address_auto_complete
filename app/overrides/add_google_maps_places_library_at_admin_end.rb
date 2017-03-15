c8a92092403df692c51fa0b1690716d45ee010a0
PareshGupta <paresh@vinsol.com>
2017-03-15 12:53:18 +0530

    fix issue - included google map library multiple times on admin interface

--- app/overrides/add_google_maps_places_library_at_admin_end.rb
+++ app/overrides/add_google_maps_places_library_at_admin_end.rb
@@ -1,6 +1,13 @@
 Deface::Override.new(
-  virtual_path: 'spree/admin/shared/_address_form',
+  virtual_path: 'spree/admin/users/_addresses_form',
   name: 'add_google_maps_places_library_at_admin_end',
-  insert_after: "[data-hook='address_fields']",
+  insert_before: "[data-hook='bill_address_wrapper']",
+  partial: 'spree/shared/add_google_maps_places_library'
+)
+
+Deface::Override.new(
+  virtual_path: 'spree/admin/orders/customer_details/_form',
+  name: 'add_google_maps_places_library_at_admin_end',
+  insert_before: "[data-hook='bill_address_wrapper']",
   partial: 'spree/shared/add_google_maps_places_library'
 )
